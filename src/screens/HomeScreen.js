ffc792b9c65c3e5f98ea7895be8c16198737c5a4
NotPerr <83740861+NotPerr@users.noreply.github.com>
2022-04-23 18:03:14 +0800

    change color

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,17 +15,17 @@ const HomeScreen = () => {
     return (
         <NativeBaseProvider>
             <SafeAreaProvider>
-                <SafeAreaView flex={1}>
-                    <Center flex={1}>
+                <SafeAreaView flex={1} >
+                    <Center flex={1} bgColor={"#711F8C"}>
                         <HStack space={20}>
-                            <Button borderRadius={0} width={70} onPress = {() => dispatch(increaseOne())}>
+                            <Button bgColor={"#2C8C1F"} borderRadius={0} width={70} onPress = {() => dispatch(increaseOne())}>
                                 <Text fontSize={40} color="white">+</Text>
                             </Button>
-                            <Button borderRadius={0} width={70} onPress = {() => dispatch(decreaseOne())}>
+                            <Button bgColor={"#F08B18"} borderRadius={0} width={70} onPress = {() => dispatch(decreaseOne())}>
                                 <Text fontSize={40} color="white">-</Text>
                             </Button>
                         </HStack>
-                        <Text fontSize={40} mt={20} color="black">
+                        <Text fontSize={40} mt={20} color="white">
                             {counterValue}
                         </Text>
                     </Center>
